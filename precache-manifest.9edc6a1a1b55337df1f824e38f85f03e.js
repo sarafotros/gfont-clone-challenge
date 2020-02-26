61e8a105c7a8a8bfc5b17b65d2edccde15f920ab
Dan Krishnan <dan@wearecodenation.com>
2020-02-26 14:47:57 +0000

    Updates

---
+++ precache-manifest.9edc6a1a1b55337df1f824e38f85f03e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0312dbdf5cfc5e17ab2019ae2ac6b902",
+    "url": "/gfont-clone-challenge/index.html"
+  },
+  {
+    "revision": "1f375de3f45c6e3d6f4d",
+    "url": "/gfont-clone-challenge/static/css/main.75accb77.chunk.css"
+  },
+  {
+    "revision": "704337cc40ce26218fdd",
+    "url": "/gfont-clone-challenge/static/js/2.46c95301.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gfont-clone-challenge/static/js/2.46c95301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f375de3f45c6e3d6f4d",
+    "url": "/gfont-clone-challenge/static/js/main.18c054b9.chunk.js"
+  },
+  {
+    "revision": "bfaf37823fd7c9c49668",
+    "url": "/gfont-clone-challenge/static/js/runtime-main.766beadf.js"
+  },
+  {
+    "revision": "ace77d1b968c50fbe27fee3c1860bb34",
+    "url": "/gfont-clone-challenge/static/media/plus.ace77d1b.svg"
+  },
+  {
+    "revision": "744cf00fad7c7346a7062af426a40cc8",
+    "url": "/gfont-clone-challenge/static/media/searchIcon.744cf00f.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gfont-clone-challenge/precache-manifest.9edc6a1a1b55337df1f824e38f85f03e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gfont-clone-challenge/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
